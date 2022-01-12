428037bc37da1fa2237f64859e88c40939ee06c2
Jerso Sando <jerso.sando@gmail.com>
2022-01-11 19:04:43 -0700

    final commit

--- server/controller.js
+++ server/controller.js
@@ -1,18 +1,45 @@
 const houses = require("./db.json")
+let globalId = 4
 
 
 
 module.exports = {
     getHouses: (req, res) => {
-// code here
+        res.status(200).send(houses)
     },
     deleteHouse: (req, res) => {
-        // code here
+        let index = houses.findIndex(elem => elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
     },
     createHouse: (req, res) => {
-        // code here
+        let {address, price, imageURL} = req.body
+        let newHouse = {
+            id: globalId,
+            address,
+            price,
+            imageURL
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+        globalId++
     },
     updateHouse: (req, res) => {
-        // code here
+        let {id} = req.params
+        let {type} = req.body
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if (houses [index].price <= 10000 && type === 'minus') {
+            houses[index].price = 0
+            res.status(200).send(houses)
+        } else if (type === 'plus') {
+            houses[index].price += 10000
+            res.status(200).send(houses)
+        } else if (type === 'minue') {
+            houses[index].price -= 10000
+            res.status(200).send(houses)
+        } else{
+            res.sendStatus(400)
+        }
     }
 }
--- server/index.js
+++ server/index.js
@@ -13,4 +13,9 @@ const {
     updateHouse
 } = require ('./controller')
 
+app.get('/api/houses', getHouses);
+app.post('/api/houses', createHouse)
+app.put('/api/houses/:id', updateHouse)
+app.delete('api/houses/:id', deleteHouse)
+
 app.listen(4004, ()=> console.log('Server running on 4004'))
